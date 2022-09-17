23c709364205110a8692f19900a85b564245b6df
kishor <kishodev36@gmail.com>
2022-09-17 14:35:07 +0530

    update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 import pickle
 from flask import  Flask,request,app,jsonify,url_for,render_template
 import numpy as np
@@ -22,6 +23,14 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()] 
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))   
 
 
 if __name__=='__main__':
